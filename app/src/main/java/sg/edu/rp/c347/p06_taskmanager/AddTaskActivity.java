3f9614af607c46bb05f6a5b7a6473cd0b43641aa
Kiraow188 <17010368@myrp.edu.sg>
2019-05-28 12:35:46 +0800

    update

---
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/AddTaskActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c347.p06_taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    EditText etName, etDesc;
+    Button btnAdd, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        etDesc = findViewById(R.id.etDesc);
+        etName = findViewById(R.id.etName);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnCancel = findViewById(R.id.btnCancel);
+    }
+}
