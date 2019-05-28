616b94780b9f763cf70366c6bec038784e7ebc8f
Kiraow188 <17010368@myrp.edu.sg>
2019-05-28 12:21:17 +0800

    update

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager/Task.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/Task.java
@@ -1,4 +1,37 @@
 package sg.edu.rp.c347.p06_taskmanager;
 
 public class Task {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public Task(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
