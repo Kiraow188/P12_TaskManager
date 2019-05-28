4f4dc8fd975e21be4eac0f1df811acc918b78a6e
Kiraow188 <17010368@myrp.edu.sg>
2019-05-28 12:17:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/MainActivity.java
@@ -0,0 +1,26 @@
+package sg.edu.rp.c347.p06_taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvTask;
+    Button btnAdd;
+    ArrayList<Task> tasks;
+    ArrayAdapter aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvTask = findViewById(R.id.lvTask);
+        btnAdd = findViewById(R.id.btnAdd);
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/Task.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c347.p06_taskmanager;
+
+public class Task {
+}
