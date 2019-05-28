60758288a34713d8049eba16f70035f226cd1baa
17010439 <abdullah_fauzan@outlook.com>
2019-05-28 13:26:58 +0800

    ??

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager/AddTaskActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/AddTaskActivity.java
@@ -1,10 +1,22 @@
 package sg.edu.rp.c347.p06_taskmanager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -37,13 +49,14 @@ public class AddTaskActivity extends AppCompatActivity {
                 DBHelper dbh = new DBHelper(AddTaskActivity.this);
                 long row_affected = dbh.insertTask(name, desc);
                 dbh.close();
-                if (row_affected != -1){
+                if (row_affected != -1) {
                     showNotification();
                     Toast.makeText(AddTaskActivity.this, "Added successfully",
                             Toast.LENGTH_SHORT).show();
                     etName.setText("");
                     etDesc.setText("");
 
+                }
             }
         });
     }
@@ -81,5 +94,5 @@ public class AddTaskActivity extends AppCompatActivity {
 
         notificationManager.notify(requestCode, n);
         finish();
-    };
+    }
 }
