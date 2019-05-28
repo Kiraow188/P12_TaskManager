36c9927c3004f6a2a868263b4452cb9b53461cb3
17010336 <17010336@myrp.edu.sg>
2019-05-28 13:49:32 +0800

    Main

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/MainActivity.java
@@ -26,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         lvTask = findViewById(R.id.lvTask);
         btnAdd = findViewById(R.id.btnAdd);
 
+        DBHelper db = new DBHelper(MainActivity.this);
+        tasks = db.getAllTasks();
+        aa = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1 ,tasks);
+        lvTask.setAdapter(aa);
+        aa.notifyDataSetChanged();
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,5 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
