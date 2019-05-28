1ab8a1ed01b1b7d4fe3b2c0d3f1f69f40acd5250
NormanLeo <17010201@myrp.edu.sg>
2019-05-28 13:09:03 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager/AddTaskActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager/AddTaskActivity.java
@@ -1,10 +1,21 @@
 package sg.edu.rp.c347.p06_taskmanager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -33,7 +44,44 @@ public class AddTaskActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
+                showNotification();
             }
         });
     }
+
+    private void showNotification(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("default", "DEFAULT Channel", NotificationManager.IMPORTANCE_HIGH);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(AddTaskActivity.this, MainActivity.class);
+        int requestCode = 888;
+        PendingIntent pIntent = PendingIntent.getActivity(AddTaskActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(AddTaskActivity.this, "default");
+        builder.setContentTitle("Amazing Offer!");
+        builder.setContentText("Subject");
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(uri);
+
+        builder.setPriority(Notification.PRIORITY_HIGH);
+
+        Notification n = builder.build();
+
+        notificationManager.notify(requestCode, n);
+        finish();
+    };
 }
